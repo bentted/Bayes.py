d82ed673cd5497ef04b3cb2a0fa4cfb50e65c2e3
bentted <152133542+bentted@users.noreply.github.com>
2024-05-13 19:38:49 -0400

    Initial commit

---
+++ class Search():.py
@@ -0,0 +1,8 @@
+class Search():
+    def __init__(self, name):
+        self.name = name
+        self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
+        if self.img is None:
+            print('Could not load map file {}'.format(MAP_FILE),
+                   file = sys.stderr)
+
